1dbb1a4bc3e8df58ceac09b806a333ba33d0c4a4
Development-JB <69400455+Development-JB@users.noreply.github.com>
2020-11-26 00:10:38 +0100

    Upload SupplyChainModel

---
+++ SupplyChain_Optimization/Python/H2forEU_config_general.py
@@ -0,0 +1,20 @@
+# General config settings
+
+# Import packages
+from pathlib import Path
+
+# Import files
+
+
+
+### File directories
+
+# Current file
+cwd = Path().absolute()
+# Current directory + 1 Level
+wd_l1 = Path().absolute().parents[0]
+# Current directory + 2 Level
+wd_l2 = Path().absolute().parents[1]
+# Current directory + 3 Level
+wd_l3 = Path().absolute().parents[2]
+
